883785c2b7b6c97b18ee25404cfdce0ade81e6a3
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-20 00:26:56 +0200

    Adding some structure to experience section

--- src/components/cv-page/main/experience/experience.js
+++ src/components/cv-page/main/experience/experience.js
@@ -5,13 +5,48 @@ import ExperienceBar from './__experience__experience-progress-bar/experience__e
 class Experience extends Component {
   render() {
     return (
-      <div className="education">
-        <div className="main__section-name experience__section-name">
-          <div className="main__section-name__icon experience__section-name__icon_experience"></div>
-          <p className="main__section-name__text experience__section-name__text_experience">experience</p>
+
+      <div className="experience">
+        <div className="experience__section experience__section_left">
+
+          <div className="experience__section__el experience__section__el_left">
+            <p className="experience__section__el__text_title">Stage Chez Artips</p>
+            <p className="experience__section__el__text_main">Travaux de design et d'interogation dans la Start-up. Francaise "Artips". Montreal, CANADA</p>
+            <div className="experience__section__el__text_date">
+              <p className="experience__section__el__text_date_month">Julliet-Aout</p>
+              <p className="experience__section__el__text_date_year">2013</p>
+            </div>
+          </div>
+
+          <div className="experience__section__el experience__section__el_left">
+            <p className="experience__section__el__text_title">Creation de Mini Rider's</p>
+            <p className="experience__section__el__text_main">Lancement du forum Mini-rider.com site comptabilisant aujourd'hui plus de 1400 visites quotidiennes</p>
+            <div className="experience__section__el__text_date">
+              <p className="experience__section__el__text_date_month">Novembre</p>
+              <p className="experience__section__el__text_date_year">2009</p>
+            </div>
+          </div>
+
         </div>
-        <ExperienceBar></ExperienceBar>
+        <div className="experience__section experience__section_right">
+
+          <div className="main__section-name experience__section-name">
+            <div className="main__section-name__icon experience__section-name__icon_experience"></div>
+            <p className="main__section-name__text experience__section-name__text_experience">experience</p>
+          </div>
+
+          <div className="experience__section__el experience__section__el_right">
+            <p className="experience__section__el__text_title">Travaux saisonier</p>
+            <p className="experience__section__el__text_main">Saisonier dans la ceuillette de mandarine. Riverton Farm - 4625 Gayndah AUSTRALIE</p>
+            <div className="experience__section__el__text_date">
+              <p className="experience__section__el__text_date_month">Mars-Mai</p>
+              <p className="experience__section__el__text_date_year">2011</p>
+            </div>
+          </div>
+        </div>
+
       </div>
+
     );
   }
 }
